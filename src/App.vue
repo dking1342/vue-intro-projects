bc304d30ddb2aa0203327d64b1f1d8fd987cf16f
David King <leanmachine42@yahoo.com>
2021-09-08 14:16:59 -0500

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    hello
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods:{
+    async getWeather(location) {
+      try {
+        let response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.VUE_APP_KEY}&q=${location}&aqi=yes`)
+        if(response.status === 200){
+          const data = await response.json();
+          console.log('data',data)
+        } else {
+          throw Error("Error when fetching")
+        }
+      } catch (error) {
+        console.log('error',error.message)
+      }
+    },
+  },
+  mounted() {
+    this.getWeather('Appleton');
   }
 }
 </script>
